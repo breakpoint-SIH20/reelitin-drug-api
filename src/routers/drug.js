13402eb7889a0a4af278c7c973440d395084fa19
mukherjeearnab <mukherjeearnab905@gmail.com>
2020-04-03 10:43:02 +0530

    added international to finder

--- src/routers/drug.js
+++ src/routers/drug.js
@@ -22,6 +22,7 @@ router.get('/drug/find', async (req, res) => {
         Drug.findOne({ $or : [
             { 'synonyms.synonym._do' : { $regex : new RegExp(name, "i") } },
             { 'products.product.dname' : { $regex : new RegExp(name, "i") } }
+            { 'international_brands.international_brand.dname' : { $regex : new RegExp(name, "i") } }
         ]}, (err, doc) => {
             if (err || doc == null) return res.sendStatus(404)
             console.log(doc)
